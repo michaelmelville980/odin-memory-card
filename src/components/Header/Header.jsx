ca17598805721fd8cdd022bad18831d7dffa3db4
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-06-12 10:54:48 -0400

    feat(header): adds header component to display game information

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ScoreBoard from "./Scoreboard/Scoreboard.jsx";
+
+export default function Header({ currentScore, roundScore }) {
+  return (
+    <header>
+      <div>
+        <h1>Animal Memory Game</h1>
+        <ScoreBoard currentScore={currentScore} roundScore={roundScore} />
+      </div>
+      <p>
+        Get points by clicking on an image but don't click on any more than
+        once!
+      </p>
+    </header>
+  );
+}
