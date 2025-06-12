c15de67f2f1b25a548e060e5fd9ff236200ad7c1
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-06-12 10:11:17 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Header from "./components/Header/Header.jsx";
+import Board from "./components/Board/Board.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Board />
+    </>
+  );
+}
